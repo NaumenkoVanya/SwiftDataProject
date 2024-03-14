09d56edda1e084b09c011afc2d99c36e9b659601
Vanya <whitewolf3310@icloud.com>
2024-03-14 21:52:55 +0300

    add new project

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Ваня Науменко on 14.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
